72c6e66a6db4a8e01eaf3a0d57ef24461f698e00
Helton Mori <heltonmori@gmail.com>
2023-10-11 00:07:17 -0300

    Assertions para metodo de soma

--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -1,3 +1,9 @@
 module.exports.sum = (num1, num2) => {
-    return parseInt(num1) + parseInt(num2)
+    const int1 = parseInt(num1, 10)
+    const int2 = parseInt(num2, 10)
+
+    if (Number.isNaN(int1) || Number.isNaN(int2)) {
+        throw new Error('Erro no input')
+    }
+    return +num1 + +num2
 }
--- src/lib/calculator.spec.js
+++ src/lib/calculator.spec.js
@@ -9,3 +9,21 @@ it('Deve somar 2+2 com resultado 4', () => {
 it('Deve somar 2+2 com resultado 4, mesmo que um dos números seja string', () => {
     expect(sum('2', '2')).toBe(4)
 })
+
+it('Deve indicar erro se o parâmetro não puder ser convertido em inteiro', () => {
+    expect(() => {
+        sum('', '2')
+    }).toThrowError()
+
+    expect(() => {
+        sum([2, 2])
+    }).toThrowError()
+
+    expect(() => {
+        sum({})
+    }).toThrowError()
+
+    expect(() => {
+        sum()
+    }).toThrowError()
+})
