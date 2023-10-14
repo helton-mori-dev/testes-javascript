da1c174cdc58bd7d12188efefbef9029d4af797e
Helton Mori <heltonmori@gmail.com>
2023-10-14 00:00:14 -0300

    Testando array como parametros

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -9,6 +9,28 @@ describe("Objeto para query string", () => {
     queryString(obj);
     expect(queryString(obj)).toBe("name=Fabio&profession=developer");
   });
+
+  it("Deve criar uma query string valida se uma array for recebida", () => {
+    const obj = {
+      name: "Fabio",
+      abilities: ["JS", "TDD"],
+    };
+    expect(queryString(obj)).toBe("name=Fabio&abilities=JS,TDD");
+  });
+
+  it("Deve apontar erro se um objeto for passado como valor", () => {
+    const obj = {
+      name: "Fabio",
+      abilities: {
+        first: "JS",
+        second: "TDD",
+      },
+    };
+
+    expect(() => {
+      queryString(obj);
+    }).toThrowError();
+  });
 });
 
 // describe('Name of the group', () => {
