c1c9690b677741567d7b3b79c5b11eed2075ba1f
Helton Mori <heltonmori@gmail.com>
2023-10-13 22:53:16 -0300

    Instalando e configurando o prettier

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,16 +1,16 @@
-const {
-    queryString
-} = require('./queryString')
+const { queryString } = require("./queryString");
 
-describe('Objeto para query string', () => {
-    it('Deve criar uma query string valida se um objeto for recebido', () => {
-        const obj = {
-            name: 'Fabio',
-            profession: 'developer'
-        }
-        queryString(obj)
-        expect(queryString(obj)).toBe(
-            'name=Fabio&profession=developer'
-        )
-    })
-})
+describe("Objeto para query string", () => {
+  it("Deve criar uma query string valida se um objeto for recebido", () => {
+    const obj = {
+      name: "Fabio",
+      profession: "developer",
+    };
+    queryString(obj);
+    expect(queryString(obj)).toBe("name=Fabio&profession=developer");
+  });
+});
+
+// describe('Name of the group', () => {
+
+// });
