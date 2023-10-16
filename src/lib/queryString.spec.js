cf8ac404476d531f5405d85087b7296e7a34b93e
Helton Mori <heltonmori@gmail.com>
2023-10-15 23:25:54 -0300

    Coverage 100% e adicionada pasta no git ignore

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,37 +1,37 @@
 const { queryString, parse } = require("./queryString");
 
-// describe("Objeto para query string", () => {
-//   it("Deve criar uma query string valida se um objeto for recebido", () => {
-//     const obj = {
-//       name: "Fabio",
-//       profession: "developer",
-//     };
-//     queryString(obj);
-//     expect(queryString(obj)).toBe("name=Fabio&profession=developer");
-//   });
+describe("Objeto para query string", () => {
+  it("Deve criar uma query string valida se um objeto for recebido", () => {
+    const obj = {
+      name: "Fabio",
+      profession: "developer",
+    };
+    queryString(obj);
+    expect(queryString(obj)).toBe("name=Fabio&profession=developer");
+  });
 
-//   it("Deve criar uma query string valida se uma array for recebida", () => {
-//     const obj = {
-//       name: "Fabio",
-//       abilities: ["JS", "TDD"],
-//     };
-//     expect(queryString(obj)).toBe("name=Fabio&abilities=JS,TDD");
-//   });
+  it("Deve criar uma query string valida se uma array for recebida", () => {
+    const obj = {
+      name: "Fabio",
+      abilities: ["JS", "TDD"],
+    };
+    expect(queryString(obj)).toBe("name=Fabio&abilities=JS,TDD");
+  });
 
-//   it("Deve apontar erro se um objeto for passado como valor", () => {
-//     const obj = {
-//       name: "Fabio",
-//       abilities: {
-//         first: "JS",
-//         second: "TDD",
-//       },
-//     };
+  it("Deve apontar erro se um objeto for passado como valor", () => {
+    const obj = {
+      name: "Fabio",
+      abilities: {
+        first: "JS",
+        second: "TDD",
+      },
+    };
 
-//     expect(() => {
-//       queryString(obj);
-//     }).toThrowError();
-//   });
-// });
+    expect(() => {
+      queryString(obj);
+    }).toThrowError();
+  });
+});
 
 describe("QueryString para objeto", () => {
   it("Deve converter uma query string em objeto", () => {
